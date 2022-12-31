09f365f1f072cf7f9091f23eb5a21b80ccb37d42
umairian <itsumairsyed@gmail.com>
2022-12-31 14:30:09 +0500

    Additional user routes added

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,70 @@ module.exports = {
       return res.status(200).send({ users });
     } catch (err) {
       console.log(err);
-      return res.status(err.status || 500).message(err.message || "Something went wrong...");
+      return res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong!");
     }
-  }
+  },
+  getOne: async (req, res) => {
+    try {
+      const { user } = req;
+      return res.status(200).send({ user });
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong!");
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const { name, email } = req.body;
+
+      if (!name || !email) {
+        throw { status: 400, message: "Required fields can't be empty" };
+      }
+
+      const createdUser = await Users.create({
+        name,
+        email,
+      });
+
+      return res.status(200).send({ user: createdUser });
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong!");
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { user, body } = req;
+      const { name } = body;
+
+      if (!name) {
+        throw { status: 400, message: "Required fields can't be empty" };
+      }
+
+      await Users.update(
+        {
+          name,
+        },
+        {
+          where: {
+            id: user.id,
+          },
+          individualHooks: true,
+        }
+      );
+
+      return res.status(200).send("User updated successfully");
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(err.status || 500)
+        .message(err.message || "Something went wrong!");
+    }
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 
+// Middlewares
+const authenticateUser = require("../middlewares/authenticate_user");
+
 // Controllers
 const controller = require("../controllers/user");
 
-router.use("/users", controller.get);
+// Routes
+const blogRouter = require("./blog");
+
+router.route("/").post(controller.create).get(controller.get);
+router.use("/:userId/blogs", authenticateUser, blogRouter);
+router
+  .route("/:userId")
+  .all(authenticateUser)
+  .get(controller.getOne)
+  .put(controller.update);
 
 module.exports = router;
